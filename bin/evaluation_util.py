6241d47203dfec582946a54fe38b2cab77d26b5f
Keisuke Ogaki <17926501+kogaki@users.noreply.github.com>
2018-03-13 13:50:49 +0900

    PCK(150mm以内正解率)の計測 (#14)
    * :star: MPII Inf dataset
    
    * :art: camera params
    
    * :star: start learning
    
    * :bug: matlab is 1-origin ...
    
    * :art: visualize mpii3d
    
    * :bug: compatible with numpy < 1.14
    
    * :star: evaluate PCK

---
+++ bin/evaluation_util.py
@@ -0,0 +1,5 @@
+class JointsForPCK(object):
+    """
+    @see Monocular 3D Human Pose Estimation In The Wild Using Improved CNN Supervision
+    """
+    from_h36m_joints = [1, 2, 3, 4, 5, 6, 8, 10, 11, 12, 13, 14, 15, 16]
